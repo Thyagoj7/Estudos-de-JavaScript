14744be9491efd18aa1da53d57da89d000ca39d9
Thyago <t.y_josef7@hotmail.com>
2021-01-02 19:25:54 -0300

    Acressimo de codigos e a pasta desafio da DIO

---
+++ FunçãoMath.js
@@ -0,0 +1,21 @@
+var aluno =[];
+var x = aluno.length;
+function Random(x){
+    return Math.floor(Math.random() * x + 1);
+}
+
+for(var i = 0; i < 1; i++){
+    x=(Random(100));
+    console.log("O tamanho do vetor x e " + x);
+}
+
+
+
+
+
+
+
+
+
+
+
